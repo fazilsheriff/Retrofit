0a77412604b20de112f2f797f62730fb77962241
fazil.s@lantrasoft.com <SinCos123>
2020-10-16 14:57:05 +0530

    logging the resposne

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -1,3 +1,20 @@
+/// Todo 1: Simmple Get
+/// Todo 2:  Get By ID
+/// Todo 3: All logs
+/// Todo 4: Get By URL
+/// Todo 5: Get By ID
+/// Todo 6: Get By Query
+/// Todo 7: Get By Query Map
+/// Todo 8: Simmple Post by Body
+/// Todo 9: Post by Field
+/// Todo 10: Post by Form URL Encoded
+/// Todo 11: Put
+/// Todo 12: patch
+/// Todo 13: Delete
+/// Todo 14: Header
+/// Todo 15: Networking Interceptors
+
+
 package com.example.retrofit
 
 import androidx.appcompat.app.AppCompatActivity
